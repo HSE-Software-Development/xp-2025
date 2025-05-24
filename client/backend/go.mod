357dbdf6946dac3d84c3a63b71eb36b8d240abe2
kohlrabka <DmitriyVlasovDV1@yandex.ru>
2025-05-24 18:47:16 +0300

    Chat

---
+++ client/backend/go.mod
@@ -0,0 +1,5 @@
+module github.com/HSE-Software-Development/xp-2025
+
+go 1.24.2
+
+require github.com/gorilla/websocket v1.5.3
---
+++ client/backend/main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true // В продакшене нужно ограничить домены!
+	},
+}
+
+type Client struct {
+	conn *websocket.Conn
+	room string
+	name string
+}
+
+type Request struct {
+	Type   string `json:"type"` // join, message, leave
+	Room   string `json:"room"`
+	Sender string `json:"sender"`
+	Text   string `json:"text"`
+}
+type Message struct {
+	Room   string
+	Sender string `json:"sender"`
+	Text   string `json:"text"`
+}
+
+var clients = make(map[*Client]bool)
+var broadcast = make(chan Message)
+
+func main() {
+	http.HandleFunc("/ws", handleConnections)
+	go handleMessages()
+
+	log.Println("Сервер запущен на :8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("Ошибка сервера: ", err)
+	}
+}
+
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ws.Close()
+
+	client := &Client{conn: ws}
+	clients[client] = true
+
+	for {
+		var req Request
+		err := ws.ReadJSON(&req)
+		if err != nil {
+			log.Printf("Ошибка: %v", err)
+			break
+		}
+
+		var msg Message
+
+		switch req.Type {
+		case "join":
+			client.room = req.Room
+			client.name = req.Sender
+			msg.Room = req.Room
+			msg.Sender = "Система"
+			msg.Text = req.Sender + " присоединился к чату"
+		case "message":
+			msg.Room = client.room
+			msg.Sender = req.Sender
+			msg.Text = req.Text
+		}
+		fmt.Println("Got request:")
+		fmt.Println(req)
+		fmt.Println("Sending message:")
+		fmt.Println(msg)
+
+		broadcast <- msg
+	}
+}
+
+func handleMessages() {
+	for {
+		msg := <-broadcast
+
+		for client := range clients {
+			if client.room == msg.Room {
+				err := client.conn.WriteJSON(msg)
+				if err != nil {
+					log.Printf("Ошибка: %v", err)
+					client.conn.Close()
+					delete(clients, client)
+				}
+			}
+		}
+	}
+}
